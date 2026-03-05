656fc56be155c2fac595f255abe138a9bbb7435c
ParkerC555 <dus21jwu@compute025.hali.private>
2026-03-05 13:40:03 +0000

    First draft complete sbatch script read alignment

--- download_denisovan_cp.sh
+++ download_denisovan_cp.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #SBATCH --job-name=download_denisovan
 #SBATCH --time=05:00:00
@@ -7,6 +8,14 @@
 #SBATCH -e /gpfs/home/dus21jwu/scratch/DSB-26-Group2/Error_Messages/download_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dus21jwu@uea.ac.uk
+
+#this sbatch file needs renaming- we should carefully consdier all of our naming conventions
+#job names  and other details can definitely be improved
+#final script  could have a generic email-  this will avoid me being emailed when they test it lol
+#can someone else test the email part as it doesn't work for
+#could include further directories for tidier data
+#index own reference genome  
+#remove these comments for final commit
 
 #module load
 module load  bwa/0.7.19
@@ -30,28 +39,17 @@ for id in ERR145618 ERR145620 ERR145622 ERR145624; do
     wget -nc -P "$rawdata" ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR145/${id}/${id}_2.fastq.gz
 done
 
-# Copy and gunzip into reference directory
-for f in "$rawdata"/*.fastq.gz; do
-    if [ -f "${reference}/$(basename ${f%.gz})" ]; then
-        echo "Skipping $(basename $f), already unzipped"
-    else
-        cp "$f" "$reference/"
-        gunzip "$reference/$(basename $f)"
-    fi
-done
-
-
 for id in ERR145618 ERR145620 ERR145622 ERR145624; do
 #align reads to reference
     bwa mem "/gpfs/data/BIO-DSB/Session4/ref/human-ref-GRCh38.fasta" "${rawdata}/${id}_1.fastq.gz" "${rawdata}/${id}_2.fastq.gz" > "${mappedsam}/${id}.sam"
 #convert SAM to BAM
     samtools view -b "${mappedsam}/${id}.sam" > "${mappedsam}/${id}.bam"
 #sort BAM
-    samtools sort -@ 4 -o "${mappedsam}/${id}.sorted.bam"${mappedsam}/${id}.bam"" "${mappedsam}/${id}.bam"
+    samtools sort -@ 4 -o "${mappedsam}/${id}.sorted.bam" "${mappedsam}/${id}.bam"
 #index BAM
     samtools index "${mappedsam}/${id}.sorted.bam"
 #extract mapping statistics
     samtools flagstat "${mappedsam}/${id}.sorted.bam"
 #coverage and sex karyotype determination
-    samtools coverage "${mappedsam}/${id}.sorted.bam" | sort -k7r | column -t
+    samtools coverage "${mappedsam}/${id}.sorted.bam" | sort -k7r | column -t > "${mappedsam}/${id}_coverage.txt"
 done
